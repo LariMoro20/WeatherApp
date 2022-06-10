6547389808d9c20ef7f1aa8f220938d954e761ab
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-06-09 23:05:46 -0300

    [UPDATE] Pagina de ajuda e menu

--- src/App.js
+++ src/App.js
@@ -4,22 +4,11 @@ import Home from './pages/Home';
 import Help from './pages/Help';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import MetaTags from 'react-meta-tags';
 function App() {
 
 
   return (
     <Router>
-      <MetaTags>
-        <title>WeatherApp</title>
-        <meta name="description" content="Aplicação de verificação de previsão do tempo utilizando API externa." />
-        <meta property="author" content="Larissa Santos" />
-        <meta property="og:title" content="WeatherApp" />
-        <meta property="og:image" content="./images/logow.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;600&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-      </MetaTags>
       <div className="weather_main">
         <Header />
         <div className="container">
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './index.css'
+import { Link } from 'react-router-dom'
+
 export default function Header() {
   return (
     <>
       <div className="col-md-12 weather__header">
-        <div className='text-center'><img src="./images/logow.png" alt='WeatherAPP logo' className='weather__header-logo' /></div>
+        <div className='text-center'>
+          <img src="./images/logow.png" alt='WeatherAPP logo' className='weather__header-logo' />
+
+
+        </div>
+        <div className="col-md-12 text-center">
+          <nav className='navegation'>
+            <ul >
+              <li> <Link to="/" className=''>Inicial</Link></li>
+              <li><Link to="/help" className=''>Ajuda</Link></li>
+            </ul>
+          </nav>
+
+
+
+        </div>
+
       </div>
     </>
   )
